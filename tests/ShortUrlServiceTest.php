812beb39d73e7958d3190e7530beb5430ae4001d
Angelos Roussakis <aggelos.roussakis@gmail.com>
2018-09-24 11:47:50 +0300

    Tests and Bug fixes

---
+++ tests/ShortUrlServiceTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asenlog
+ * Date: 24/09/2018
+ * Time: 09:41
+ */
+
+namespace Tests;
+
+use App\Services\ShortUrlServiceTest;
+use PHPUnit\Framework\TestCase;
+
+class ShortUrlServiceTest extends TestCase
+{
+
+}
